c070fd535f83df661444826faa3fdc857409ef97
dheeraj1566 <dheerajjangid013@gmail.com>
2025-01-24 16:34:40 +0530

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+// import studentRouter from "./routes/studentRouter.js";
+import express from "express";
+import "dotenv/config";
+import connectToDB from "./connection.js";
+
+
+
+const PORT = 8081;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+await connectToDB();
+
+app.listen(PORT, () => console.log("Server started at port " + PORT));
+
+app.use("/patient", patientRouter);
